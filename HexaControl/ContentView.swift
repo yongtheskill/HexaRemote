e3aadc51caa84e471376c77c52bea969192c5794
yongtheskill <yongee2003@gmail.com>
2021-07-31 17:10:49 +0800

    Initial Commit

---
+++ HexaControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HexaControl
+//
+//  Created by Yong Ee on 31/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HexaControl/HexaControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexaControlApp.swift
+//  HexaControl
+//
+//  Created by Yong Ee on 31/7/21.
+//
+
+import SwiftUI
+
+@main
+struct HexaControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
